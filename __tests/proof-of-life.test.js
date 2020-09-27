d9bb6f7b90ab2d6f2dff237465c504ae812b16b8
Joe Pennock <pennockjoe@gmail.com>
2020-09-27 00:53:30 -0700

    add starter files, initialized npm project and set up scripts in package.json, added a simple proof of life test for ci/cd purposes

---
+++ __tests/proof-of-life.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+this is simply to keep github actions 
+from failing with each push prior to 
+proper tests being writen */
+
+describe('Proof of life', () => {
+
+  it('should always pass', () => {
+    expect(true).toBeTruthy();
+  });
+  
+});
